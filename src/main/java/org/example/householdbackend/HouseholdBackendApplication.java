9ad69ea60a003d80b399cb448ce753621a031125
mshahnaza <mamirovasahnaza042@gmail.com>
2025-12-17 16:06:10 +0600

    init commit

---
+++ src/main/java/org/example/householdbackend/HouseholdBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.householdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseholdBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseholdBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/householdbackend/HouseholdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.householdbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseholdBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
